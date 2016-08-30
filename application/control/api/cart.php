12443f15c7ef2b5db48643bde28ac6c3d35ac856
jincc <326550324@qq.com>
2016-08-30 14:33:54 +0800

    购物车的捆绑解决掉了

--- application/control/api/cart.php
+++ application/control/api/cart.php
@@ -63,7 +63,6 @@ class cart extends common
                     'product.stock',
                     'product.auto_stock',
                     'product.freetax',
-                //	'cart.bind',
                 ],
             ];
 
@@ -103,13 +102,13 @@ class cart extends common
                     }
                     
                     //捆绑销售的单价
-                    /* $priceInBind = $productHelper->getPriceByBind($p);
+                    $priceInBind = $productHelper->getPriceByBind($p);
                     if ($priceInBind)
                     {
                     	$p['price'] = $priceInBind['price'];
                     	$p['v1price'] = $priceInBind['v1price'];
                     	$p['v2price'] = $priceInBind['v2price'];
-                    } */
+                    }
 
 					//计算总价
 					switch ($user['vip']) {
