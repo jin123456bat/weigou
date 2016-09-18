cb9343247d5e0b63874978c3598e479c13f6c7f1
copy <164689681@qq.com>
2016-09-18 16:09:51 +0800

    帮顶上级邀请人

--- application/control/ajax/user.php
+++ application/control/ajax/user.php
@@ -846,8 +846,10 @@ class user extends ajax
 
         $invit_user = $this->model('user')->where('invit=?', [$invit])->find();
         if (!empty($invit_user)) {
-            if ($invit_user['vip'] == 0)
+            if ($invit_user['vip'] == 0 && $invit_user['school']==0) {
+
                 return new json(json::PARAMETER_ERROR, '邀请码错误');
+            }
 
             if ($invit_user['master'] == 1) {
                 $o_master = $invit_user['id'];
